161444080addcb6c2e22f2ee5ef3e9390b156504
profslvo <profslvo@ulife.com.br>
2023-09-25 21:15:17 -0300

    Aula 05

--- AULA04/exercicio/exercicio.java
+++ AULA04/exercicio/exercicio.java
@@ -12,24 +12,31 @@ import java.util.Scanner;
 
 public class exercicio {
     public static void main(String[] args) {
+    final int QTDE_NOTAS = 10;
     Scanner teclado = new Scanner(System.in);
         
     //criação de um vetor chamado notas, com 10 posições
-    double notas[] = new double[10];
-    double media;
-    int i = 0;
+    double notas[] = new double[QTDE_NOTAS];
+    double media,soma;
 
-    for(i = 0; i < notas.length; i++){
+    soma = 0;
+    for(int i = 0; i < notas.length; i++) {
     System.out.println("Digite a " + (i + 1) + "º nota");
     notas[i] = teclado.nextDouble();
+    soma = soma + notas[i];
    
 }
- //for(i = 0; i < notas.length; i++){
- //System.out.print(notas[i] + " ");
+    media = soma / QTDE_NOTAS;
+    System.out.println("A média da turma é " + media);
+
+    System.out.println("Notas Maiores que a média: ");
+    for(int i = 0; i < notas.length; i++){
+    if(notas[i] > media) {
+    System.out.print(notas[i] + " ");
+}
+//for(i = notas.length - 1; i >= 0; i--){
+    //System.out.printf(notas[i] + " ");
 //}
-    for(i = notas.length - 1; i >= 0; i--){
-    System.out.printf(notas[i] + " ");
 }
-}
-}
- 
+ }
+ }
